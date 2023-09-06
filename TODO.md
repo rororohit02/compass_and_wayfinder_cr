594a06830a5db0a68acdd90789d3d18fceaf0e47
Reid Smith <77130791+Retrix165@users.noreply.github.com>
2023-09-06 12:44:36 -0500

    add basic animation support for test data with queue

--- TODO.md
+++ TODO.md
@@ -2,7 +2,7 @@
 
 ## General Items
 - [x] Make TODO List
-- [ ] Format code better
+- [x] Format code better
 
 ## Wayfinder.py
 - [ ] Modify code to be a grid that represents 30 degrees horizontal and 30 degrees vertical (Does not utilize AR) and make the universal angles change as the user rotates
@@ -13,3 +13,6 @@
 - [ ] Implement latitude and longitude coordinate and translate to Cartesian using ESRI maps and MATLAB.
 - [x] Find a way to remove the grid so that only the vector is graphed without any axis
 - [ ] Loop code to run indefinitely given coordinates
+- [x] Add queue and basic 'animation'
+- [ ] Improve animation running time/efficiency
+- [ ] Add support for asynchronous data input
--- compass.py
+++ compass.py
@@ -18,36 +18,72 @@ Function:   Takes any universal point and creates a unit vector with
 
 #TODO: Loop code to run indefinitely
 
-#TODO: Format code better
+#TODO: Format code better DONE
+
+#TODO: Add support for async data input
+#       https://realpython.com/python-async-features/
+#       https://realpython.com/async-io-python/
+
+#TODO: Use more efficient python math/matrix package eventually
+#       Numpy?
+#       OpenCV?
+
+#TODO: Improve animation method for updating graph
+
 
 
 import math
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.animation import FuncAnimation
+#import numpy as np
+import queue
 #import random #To be used with random coordinates in the future
+from time import sleep
 
+class coord:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.z = 0
+        
+    def __init__(self, coord: tuple):
+        assert(len(coord) == 3)
+        self.x, self.y, self.z = coord
+    
+    def magnitude(self):
+        return math.sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
 
+    def get_unit_vector(self) -> tuple:
+        mag = self.magnitude()
+        return (self.x / mag, self.y / mag, self.z / mag)
+
+    def __str__(self):
+        return "Coord x, y, z: "+ str(self.x) + " " + str(self.y) + " " + str(self.z)
+    
+#Not used in this branch
 def setup_graph():
     plt.switch_backend('TkAgg') # Recommended for plotting vectors in a 3d space
     fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+    #ax = fig.add_subplot(111, projection='3d')
+    ax = fig.add_suplot(projection='3d')
     
     #Unit vector: Vector length (magnitude) will not exceed 1.
     
-    ax.set_xlim([-1, 1]) #Set limits to max length of unit vector for all 3 axes.
-    ax.set_ylim([-1, 1])
-    ax.set_zlim([-1, 1])
+    #ax.set_xlim([-1, 1]) #Set limits to max length of unit vector for all 3 axes.
+    #ax.set_ylim([-1, 1])
+    #ax.set_zlim([-1, 1])
     
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('z')
     
-    plt.axis('off') #Turns off axes and grid
+    #plt.axis('off') #Turns off axes and grid
 
     plt.title('Compass')
     return fig, ax
 
-
+#Not used in this branch
 def get_user_input():
     # Get cartesian coordinates from origin. Represents vector between two
     # geographical coordinates with origin being observer's coords, along with
@@ -59,8 +95,8 @@ def get_user_input():
     azimuth = int(input('Enter initial horizontal angle facing: '))
     return x, y, z, azimuth
 
-
-def generate_unit_vector(coordinate: tuple, azimuth: int) -> tuple:
+#Not used in this branch
+def generate_unit_vector(coordinate: tuple) -> tuple:
     assert(len(coordinate) == 3)
     x,y,z = coordinate
     magnitude = math.sqrt((x*x)+(y*y)+(z*z))
@@ -71,12 +107,45 @@ def generate_unit_vector(coordinate: tuple, azimuth: int) -> tuple:
 if __name__ == "__main__":
     print("START")
 
-    fig, ax = setup_graph()
-    x, y, z, azimuth = get_user_input()
-    origin = (0,0,0)
-    unit_vector = generate_unit_vector((x,y,z), azimuth)
-    ax.quiver(*origin, *unit_vector, ) #Plot vector 
-    ax.view_init(30,azimuth-90)  #Orient graph along x-axis without adjustment for azimuth
-    plt.show()
+    #Queued Animated Dots
+    plt.switch_backend('TkAgg') # Recommended for plotting vectors in a 3d space
 
+    #Setup simple 3-D fig/x
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    #Set limits and views
+    ax.set_xlim([-1,11])
+    ax.set_ylim([-1,11])
+    ax.set_zlim([-1,11])
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+    azimuth = 45
+    ax.view_init(30,azimuth-90)
+    plt.title('Compass')
+    
+    #Set up simple queue and enumerate test coordinate data
+    q = queue.SimpleQueue()
+    coords_data = [(0,0,0), (10,0,0), (10, 10, 0), (0, 10, 0), (0, 10, 10), (0, 0, 10), (10, 0, 10), (10, 10, 10)]
+    for coord_data in coords_data:
+        q.put(coord(coord_data))
+    x_data = []
+    y_data = []
+    z_data = []
+
+    #Loop through queue, update data as new coords dequeued, and redraw plot
+    for _ in range(q.qsize()):
+        new_coord = q.get()
+
+        #Update data
+        x_data.append(new_coord.x) 
+        y_data.append(new_coord.y)
+        z_data.append(new_coord.z)
+
+        #Draw data with pause after
+        ax.scatter(x_data, y_data, z_data)
+        plt.draw() 
+        plt.pause(.5)
+    
     print("FINISH")
